84f32f6548fa74ea7bb6d781b7e0cf9c60da81a8
koljastraub@protonmail.com <#Mdsjwfaikdiz9>
2020-05-17 21:12:30 +0200

    implemented the location tracking service but didnt tested yet

--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/Constants.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/Constants.kt
@@ -1,3 +1,4 @@
 package de.tudarmstadt.iptk.foxtrot.vivacoronia
 
-val LOCATION_ACCESS_PERMISSION_REQUEST_CODE = 56
+val LOCATION_ACCESS_PERMISSION_REQUEST_CODE = 56    // requests the permission to use location data from the user
+val LOCATION_ACCESS_SETTINGS_REQUEST_CODE = 57      // requests access to gps, wifi, cellular network to access location data from the user
--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
@@ -1,31 +1,45 @@
 package de.tudarmstadt.iptk.foxtrot.vivacoronia
 
 import android.Manifest
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
+import de.tudarmstadt.iptk.foxtrot.vivacoronia.locationTracking.LocationTrackingService
 
 class MainActivity : AppCompatActivity() {
-    // private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var TAG = "MainActivity"
+    // TODO check wheter google play services has the right version
+    // TODO add licencing for location api
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermissionsAndStartTracking()
+    }
+
+    fun checkPermissionsAndStartTracking() {
+        // TODO add in onResume method the start of the service, if the service isnt running
         // check wheter location permission was granted
         // get permissions
         val coarseLocationPermitted = ActivityCompat
             .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED
         val fineLocationPermitted = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-        // if android version bigger equal 10 then also background location access has to be permitted
-        // TODO check wheter background location access is needed because it is needed for API 29 and higher
         // check permission
         if (coarseLocationPermitted && fineLocationPermitted) {
             // start location service
-            requestLocationService()
+            requestLocationService(createBackgroundLocationRequest())
         }
         else {
             // request location permissions
@@ -33,8 +47,55 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun requestLocationService() {
-        // fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    fun createBackgroundLocationRequest() : LocationRequest? {
+        // code (with a few changes) from https://developer.android.com/training/location/change-location-settings
+        val locationRequest = LocationRequest.create()?.apply {
+            interval = 300000   // request all 5 minutes
+            priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY // can be changed to low power settings depending on what we need
+        }
+        return locationRequest
+    }
+
+    fun requestLocationService(locationRequest: LocationRequest?){
+        // code (with a few changes) from https://developer.android.com/training/location/change-location-settings
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest!!)
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        // gps, wifi etc is enabled so location tracking can be started
+        task.addOnSuccessListener { locationSettingsResponse ->
+            var intent: Intent = Intent(this, LocationTrackingService::class.java)
+            startForegroundService(intent)
+        }
+
+        // gps, wifi etc is not enabled, so location tracking cannot be startet
+        // instead gps, wifi has to be enabled
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                try {
+                    exception.startResolutionForResult(this@MainActivity,
+                        LOCATION_ACCESS_SETTINGS_REQUEST_CODE)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+
+
+    }
+
+    fun requestSingleLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+                location: Location? ->  run{
+            val longtitude = location!!.longitude
+            val latitude = location!!.latitude
+            val time = location!!.time
+            Log.v(TAG, time.toString() + " - " + longtitude.toString() + " - " + latitude.toString())
+        }
+        }
 
     }
 
@@ -50,7 +111,7 @@ class MainActivity : AppCompatActivity() {
                     requestLocationPermissions()
                 }
                 else {
-                    requestLocationService()
+                    requestLocationService(createBackgroundLocationRequest())
                 }
             }
         }
