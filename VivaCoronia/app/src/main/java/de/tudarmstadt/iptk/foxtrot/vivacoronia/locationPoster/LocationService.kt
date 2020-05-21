55181b6e9808362e9ddd5a11716f4e5d83e8b42e
Patrick Vimr <Patrick-Vimr@gmx.de>
2020-05-21 20:16:11 +0200

    added single location request functionality

--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/locationPoster/LocationService.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/locationPoster/LocationService.kt
@@ -1,11 +1,15 @@
 package de.tudarmstadt.iptk.foxtrot.locationPoster
 
+import android.Manifest
+import android.app.Activity
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import de.tudarmstadt.iptk.foxtrot.vivacoronia.Constants
 
@@ -18,21 +22,54 @@ class LocationService {
         private lateinit var fusedLocationClient : FusedLocationProviderClient
 
         fun getSingleLocation() : Constants.dataPoint? {
-            Log.v(TAG, "hallo")
-            val returnVal: Constants.dataPoint? = null
+
+            // while permission for location access is not granted, get it
+            if(!checkLocationPermissions(context)){
+                requestLocationPermissions(context as Activity)
+            }
+
+            var returnVal: Constants.dataPoint? = null
             fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
             fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
                 run {
                     val longitude = location!!.longitude
                     val latitude = location!!.latitude
                     val time = location!!.time
-                    val returnVal = Constants.dataPoint(longitude, latitude, time)
+                    returnVal = Constants.dataPoint(longitude, latitude, time)
                     Log.v(
                         TAG,
                         time.toString() + " - " + longitude.toString() + " - " + latitude.toString()
                     )
                 }
             }
+
+            fusedLocationClient.lastLocation.addOnFailureListener { exp: Exception? ->
+                Log.v(TAG,"Permission for location wasn't granted")
+            }
+
             return returnVal
         }
+
+    private fun requestLocationPermissions(activity: Activity) {
+        ActivityCompat.requestPermissions(activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            56
+        )// 56 should be the constant for LOCATION_ACCESS_PERMISSION_REQUEST_CODE, but it doesnt work
     }
+
+    /**
+     * returns True if Location access granted, False if not
+     */
+    private fun checkLocationPermissions(context: Context) : Boolean {
+        // check wheter location permission was granted
+        // since fine location is more accurate than coarse, coarse is included in fine
+        val fineLocationPermitted = ActivityCompat.
+        checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+        // check permission
+        return fineLocationPermitted
+    }
+
+
+
+}
