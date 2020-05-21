dc54f91ceabe1731ec89c00879c3e64f097d6192
patrick.siebke <patrick.siebke@stud.tu-darmstadt.de>
2020-05-21 16:25:41 +0200

    add button

--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
@@ -2,11 +2,21 @@ package de.tudarmstadt.iptk.foxtrot.vivacoronia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import de.tudarmstadt.iptk.foxtrot.locationPoster.locationServerCommunicator
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sendLocBtn = findViewById<Button>(R.id.sendLocationBtn)
+        sendLocBtn.setOnClickListener {
+            Toast.makeText(this, "Sending your position...", Toast.LENGTH_LONG).show()
+            locationServerCommunicator.sendCurrentPositionToServer()
+        }
     }
 }
--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/locationPoster/locationServerCommunicator.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/locationPoster/locationServerCommunicator.kt
@@ -1,4 +1,13 @@
 package de.tudarmstadt.iptk.foxtrot.locationPoster
 
 class locationServerCommunicator {
+
+    companion object {
+
+        fun sendCurrentPositionToServer() {
+
+        }
+
+    }
+
 }
