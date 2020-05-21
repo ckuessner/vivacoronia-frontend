8c48fd115487ef89b0bcf4c55101e19432eb9d04
Patrick Vimr <Patrick-Vimr@gmx.de>
2020-05-21 17:41:37 +0200

    functionality to get single location when pressing button, still faulty

--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
@@ -1,21 +1,26 @@
 package de.tudarmstadt.iptk.foxtrot.vivacoronia
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import de.tudarmstadt.iptk.foxtrot.locationPoster.LocationServerCommunicator
+import de.tudarmstadt.iptk.foxtrot.locationPoster.LocationService
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val currentContext : Context = this
+        val service = LocationService(currentContext)
 
         val sendLocBtn = findViewById<Button>(R.id.sendLocationBtn)
         sendLocBtn.setOnClickListener {
             Toast.makeText(this, "Sending your position...", Toast.LENGTH_LONG).show()
-            LocationServerCommunicator.sendCurrentPositionToServer()
+            service.getSingleLocation()
         }
     }
 }
--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/locationPoster/LocationService.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/locationPoster/LocationService.kt
@@ -1,4 +1,38 @@
 package de.tudarmstadt.iptk.foxtrot.locationPoster
 
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.os.IBinder
+import android.util.Log
+import com.google.android.gms.location.*
+import de.tudarmstadt.iptk.foxtrot.vivacoronia.Constants
+
 class LocationService {
-}
+        var context: Context
+        constructor(conContext: Context) {
+            context = conContext
+        }
+        private var TAG = "Location poster"
+        private lateinit var fusedLocationClient : FusedLocationProviderClient
+
+        fun getSingleLocation() : Constants.dataPoint? {
+            Log.v(TAG, "hallo")
+            val returnVal: Constants.dataPoint? = null
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                run {
+                    val longitude = location!!.longitude
+                    val latitude = location!!.latitude
+                    val time = location!!.time
+                    val returnVal = Constants.dataPoint(longitude, latitude, time)
+                    Log.v(
+                        TAG,
+                        time.toString() + " - " + longitude.toString() + " - " + latitude.toString()
+                    )
+                }
+            }
+            return returnVal
+        }
+    }
