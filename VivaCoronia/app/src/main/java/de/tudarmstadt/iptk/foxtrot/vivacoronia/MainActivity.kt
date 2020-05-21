9e656e7ffe26f787ac9f1fc56ea3fb0d1359f21a
Patrick Vimr <Patrick-Vimr@gmx.de>
2020-05-21 17:41:37 +0200

    functionality to get single location when pressing button, still faulty

--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/MainActivity.kt
@@ -1,25 +1,30 @@
 package de.tudarmstadt.iptk.foxtrot.vivacoronia
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import de.tudarmstadt.iptk.foxtrot.locationPoster.LocationServerCommunicator
+import de.tudarmstadt.iptk.foxtrot.locationPoster.LocationService
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val currentContext : Context = this
+        val service = LocationService(currentContext)
 
         // TODO: Get userID
         var userID:Int = 12345
 
         val sendLocBtn = findViewById<Button>(R.id.sendLocationBtn)
         sendLocBtn.setOnClickListener {
-            val com = LocationServerCommunicator(this, userID)
 
-            com.sendCurrentPositionToServer()
+            Toast.makeText(this, "Sending your position...", Toast.LENGTH_LONG).show()
+            service.getSingleLocation()
         }
     }
 }
