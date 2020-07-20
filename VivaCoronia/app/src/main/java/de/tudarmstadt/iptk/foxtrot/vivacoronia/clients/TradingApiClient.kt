3442ff510bcc954eb87033ff9b4688e99d5e2c10
Benjamin Laumann <benjamin.laumann@stud.tu-darmstadt.de>
2020-07-20 20:21:01 +0200

    13/14 deleted dummy list

--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/clients/TradingApiClient.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/clients/TradingApiClient.kt
@@ -14,24 +14,6 @@ import org.json.JSONArray
 import org.json.JSONObject
 
 object TradingApiClient : ApiBaseClient() {
-    private val dummyList = mutableListOf(
-        Offer("Klopapier 10 Rollen", 3, 39.90, Location(""), "4-lagiges Klopapier, vergoldet und mit Duftstoffen versehen", "1", ""),
-        Offer("Zahnpasta", 1, 3.30, Location(""), "Sorgt für strahlend weiße Zähne", "2", ""),
-        Offer("Weizenmehl 500g", 2, 2.22, Location(""), "Jeder hat es, keiner braucht es. Ich auch nicht, daher verkaufe ich will ich es los werden", "3", ""),
-        Offer("Schutzmaske", 25, 300.00, Location(""), "Ich habe vor der Pandemie alle Schutzmasken aufgekauft! Huehuehuehue!", "4", ""),
-        Offer("Klopapier 10 Rollen", 3, 39.90, Location(""), "4-lagiges Klopapier, vergoldet und mit Duftstoffen versehen", "5", ""),
-        Offer("Zahnpasta", 1, 3.30, Location(""), "Sorgt für strahlend weiße Zähne", "6", ""),
-        Offer("Weizenmehl 500g", 2, 2.22, Location(""), "Jeder hat es, keiner braucht es. Ich auch nicht, daher verkaufe ich will ich es los werden", "7", ""),
-        Offer("Schutzmaske", 25, 300.00, Location(""), "Ich habe vor der Pandemie alle Schutzmasken aufgekauft! Huehuehuehue!", "8", ""),
-        Offer("Klopapier 10 Rollen", 3, 39.90, Location(""), "4-lagiges Klopapier, vergoldet und mit Duftstoffen versehen", "9", ""),
-        Offer("Zahnpasta", 1, 3.30, Location(""), "Sorgt für strahlend weiße Zähne", "10", ""),
-        Offer("Weizenmehl 500g", 2, 2.22, Location(""), "Jeder hat es, keiner braucht es. Ich auch nicht, daher verkaufe ich will ich es los werden", "11", ""),
-        Offer("Schutzmaske", 25, 300.00, Location(""), "Ich habe vor der Pandemie alle Schutzmasken aufgekauft! Huehuehuehue!", "12", ""),
-        Offer("Klopapier 10 Rollen", 3, 39.90, Location(""), "4-lagiges Klopapier, vergoldet und mit Duftstoffen versehen", "13", ""),
-        Offer("Zahnpasta", 1, 3.30, Location(""), "Sorgt für strahlend weiße Zähne", "14", ""),
-        Offer("Weizenmehl 500g", 2, 2.22, Location(""), "Jeder hat es, keiner braucht es. Ich auch nicht, daher verkaufe ich will ich es los werden", "15", ""),
-        Offer("Schutzmaske", 25, 300.00, Location(""), "Ich habe vor der Pandemie alle Schutzmasken aufgekauft! Huehuehuehue!", "16", "")
-    )
     private val offerConverter : Klaxon = Klaxon()
 
     init {
@@ -121,5 +103,4 @@ object LocationConverter : Converter {
             throw KlaxonException("Cannot convert class ${value::class.java} with LocationConverter")
         return """ { "coordinates": [ ${value.longitude}, ${value.latitude} ], "type": "Point" } """
     }
-
 }
