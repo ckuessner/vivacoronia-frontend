49e94960ae8b9a8cc1057b347844a838b5989561
Christian Kuessner <ckuessner@mailbox.org>
2020-09-22 21:51:37 +0200

    Remove stale import after rebase

--- VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/dataStorage/Database.kt
+++ VivaCoronia/app/src/main/java/de/tudarmstadt/iptk/foxtrot/vivacoronia/dataStorage/Database.kt
@@ -2,13 +2,11 @@ package de.tudarmstadt.iptk.foxtrot.vivacoronia.dataStorage
 
 import android.content.Context
 import androidx.room.*
-import de.tudarmstadt.iptk.foxtrot.vivacoronia.Constants
 import de.tudarmstadt.iptk.foxtrot.vivacoronia.dataStorage.entities.AchievementInfo
 import de.tudarmstadt.iptk.foxtrot.vivacoronia.dataStorage.entities.DBLocation
 import de.tudarmstadt.iptk.foxtrot.vivacoronia.dataStorage.entities.QuizGame
-import de.tudarmstadt.iptk.foxtrot.vivacoronia.dataStorage.entities.User
 
-@Database(entities = [User::class, DBLocation::class, QuizGame::class, AchievementInfo::class], version = 3)
+@Database(entities = [DBLocation::class, QuizGame::class, AchievementInfo::class], version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun coronaDao() : CoronaDao
     abstract fun quizGameDao() : QuizGameDao
